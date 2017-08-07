1929da3df30c0adab1e502057cd1a3ad1a8af5b7
shidongfang <shidongfang@oradt.com>
2017-08-07 17:52:02 +0800

    111

---
+++ app/src/main/java/com/sdf/blueshopping/ui/base/ui/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.sdf.blueshopping.base;
+
+import android.app.Activity;
+
+/**
+ * Created by shidongfang on 2017/8/7.
+ */
+
+public class BaseActivity extends Activity {
+}
---
+++ app/src/main/java/com/sdf/blueshopping/ui/base/ui/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.sdf.blueshopping.ui.base.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by shidongfang on 2017/8/7.
+ */
+
+public class BaseFragment extends Fragment {
+}
